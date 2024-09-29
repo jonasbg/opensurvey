72805baff565b5ad44b49382dcb9c34ca7b37d3d
Jonasbg <1508560+jonasbg@users.noreply.github.com>
2024-09-29 20:10:39 +0200

    socket fix

--- static/js/app.js
+++ static/js/app.js
@@ -42,7 +42,8 @@
     }
 
     function connectWebSocket() {
-        socket = new WebSocket(`ws://${window.location.host}/ws`);
+        const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+        socket = new WebSocket(`${protocol}//${window.location.host}/ws`);
 
         socket.onmessage = function (event) {
             const message = JSON.parse(event.data);
